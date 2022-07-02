4d24bd0d1dd2b3ccd68a42a83c843e619be0d0b3
xasan <xasandj2002@gmail.com>
2022-07-02 18:44:00 +0500

    Mp3

---
+++ js/data.js
@@ -0,0 +1,56 @@
+let songs = [
+    {
+        artist: 'Miyagi   Эндшпиль',
+        name: 'Фая',
+        path: 'music/faya',
+        cover: 'img/Miyagi'
+    },
+    {
+        name: 'I am calling you',
+        path: 'music/I am calling you',
+        artist: 'Outlandish',
+        cover: 'img/imcallingyou'
+    },
+    {
+        name: 'Love the way',
+        path: 'music/EminamftRhina',
+        artist: 'Eminam ft Rhina',
+        cover: 'img/eminem'
+    },
+    {
+        name: 'Моя каралева',
+        path: 'music/Моя каралева',
+        artist: 'Miyagi feat Andy_Panda',
+        cover: 'img/Miyagi'
+    },
+    {
+        name: 'Бошка',
+        path: 'music/Miyagiбошка',
+        artist: 'Miyagi feat Andy_Panda',
+        cover: 'img/Miyagi'
+    },
+    {
+        name: 'Говори мне',
+        path: 'music/govorimenya',
+        artist: 'Miyagi',
+        cover: 'img/govorimenya'
+    },
+    {
+        name: 'Жить как я живу',
+        path: 'music/skriptonit',
+        artist: 'Scriptonit',
+        cover: 'img/Scriptonit'
+    },
+    {
+        name: 'Заплаканная',
+        path: 'music/Zaplakina',
+        artist: 'Miyagi & Andy Panda',
+        cover: 'img/miyagizaplaikaya'
+    },
+    {
+        name: 'Рапапам.',
+        path: 'music/MiyaGi&Ендшпиль',
+        artist: 'MiyaGi',
+        cover: 'img/Miyagi'
+    },
+]
